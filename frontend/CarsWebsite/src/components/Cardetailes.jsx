ec5fb26476ec9e9bb178f6f4409f2710b78faa02
C12-SmadiAhmed <ahmed.f.smadi@gmail.com>
2024-10-14 23:11:43 +0300

    designed the cars detailes page

--- frontend/CarsWebsite/src/components/Cardetailes.jsx
+++ frontend/CarsWebsite/src/components/Cardetailes.jsx
@@ -18,7 +18,9 @@ const [currentImageIndex, setCurrentImageIndex] = useState(0);
     const totalImages = car.carImage.length;
     console.log(totalImages)
     
-   
+    const convince=car.Convenience.split(/ +/)
+    const fixedConvince=convince.join(', ')
+
     const nextImage = () => {
         setCurrentImageIndex((prevIndex) => (prevIndex + 1) % totalImages);
       };
@@ -29,23 +31,60 @@ const [currentImageIndex, setCurrentImageIndex] = useState(0);
       console.log(car.carImage)
       return (
         <>
-      
-      
+        <div className='cardetailsmain'>
+
+
         <div className='car-details-page'>
-          <h2>{car.made}</h2>
-          <h3>Condition: {car.carCondtion}</h3>
-          <h3>Price: ${car.price}</h3>
-          <div className='image-container'>
-            <img
+        <img
               src={car.carImage[currentImageIndex]}
               alt={`Image ${currentImageIndex + 1}`}
-              className='car-image'/>
+              className='detaild-car'/>
              
-          </div>
           <button className='favbutton' onClick={prevImage}>Previous</button>
               <button className='favbutton' onClick={nextImage}>Next</button>
-          
-        </div>
+          <h2>{car.model} {car.made}</h2>
+          <h3>Condition: {car.carCondtion}</h3>
+          <h3>Price: ${car.price}</h3>
+          <br/>
+          <h1>Basics</h1>
+          <span className='feature'> <span className='label'>Exterior Color:</span>
+          <span className='value'>{car.exteriorColor}</span> </span><span className='border'></span>
+
+          <span className='feature'><span className='label'>Interior Color:</span>
+          <span className='value'>{car.interiorColor}</span>
+         </span>
+         <span className='border'></span>
+
+<span className='feature'>
+    <span className='label'>Drivetrain:</span>
+    <span className='value'>{car.drivetrain}</span>
+</span>
+<span className='border'></span>
+
+<span className='feature'>
+    <span className='label'>Transmission:</span>
+    <span className='value'>{car.Transmission}</span>
+</span>
+<span className='border'></span>
+
+<span className='feature'>
+  <span className='label'>Range:</span>
+    <span className='value'>{car.range} Km</span>
+</span>
+<span className='border'></span>
+<br/>
+<h1>Features</h1>
+<span className='feature'> <span className='label'>Convenience</span>
+<span className='value'>{fixedConvince}</span> </span><span className='border'></span>
+
+
+
+ </div> 
+         
+          <div className='contact-seller'>
+<input placeholder='contact seller'/>
+</div>
+          </div>
         </>
       );
     };
